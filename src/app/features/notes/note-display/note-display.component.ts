c90237feb1dd598080efdb012804d122412479ad
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2025-01-18 20:43:20 +0100

    docs: add todo for the NoteDisplayComponent

--- src/app/features/notes/note-display/note-display.component.ts
+++ src/app/features/notes/note-display/note-display.component.ts
@@ -1,21 +1,20 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NotesStoreService } from '../notes-store.service';
-import { Note } from '../../../core/models/note.model';
+
 @Component({
   imports: [FormsModule],
   selector: 'app-note-display',
   templateUrl: './note-display.component.html',
   styleUrls: ['./note-display.component.css'],
 })
+// TODO: fix signal misuse that works, understand why
 export class NoteDisplayComponent {
   notesStore = inject(NotesStoreService);
-
   get noteContent(): string {
     return this.notesStore.selectedNote$()?.content || '';
   }
 
-  // Update the selected note's content
   set noteContent(value: string) {
     const selectedNote = this.notesStore.selectedNote$();
     if (selectedNote) {
